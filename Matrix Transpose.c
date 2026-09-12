b3616c8174ce382b07cd85dad8ece08d771ff2ad
Sadiya <sadiya.fazil.cse.2025@miet.ac.in>
2026-09-12 20:53:27 +0530

    Add files via upload

---
+++ Matrix Transpose.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main () {
+    int a[10][10], b[10][10];
+    int r, c, i, j;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter matrix:\n");
+    for (i=0; i<r; i++) {
+        for (j=0; j<c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (i=0; i<r; i++) {
+        for (j=0; j<c; j++) {
+            b[j][i] = a[i][j];
+        }
+    }
+    printf("Transpose:\n");
+    for (i=0; i<c; i++) {
+        for (j=0; j<r; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
