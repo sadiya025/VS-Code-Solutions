b3444a2364558b0a0554d4419b1750f0799c990c
Sadiya <sadiya.fazil.cse.2025@miet.ac.in>
2026-09-12 17:17:44 +0530

    Add files via upload

---
+++ LinearSearch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main () {
+    int a[100], n, key;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements: ");
+    for (int i=0; i<n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    for (int i=0; i<n; i++) {
+        if (a[i] == key) {
+            printf("Element found at position %d", i+1);
+            return 0;
+        }
+    }
+    printf("Element not found\n");
+    return 0;
+}
